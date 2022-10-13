7e0f052f60156cb4e30ca770e945d293f159562b
cadeponder <89998363+cadeponder@users.noreply.github.com>
2022-10-12 21:55:51 -0600

    assignment8

---
+++ assignments/main.js
@@ -0,0 +1,59 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+// 2. RAW TEXT STRINGS
+
+const storyText = 'It was 94 fahrenheit outside, so :insertx: sat in water fully clothed. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+
+const insertX = ['Sarah Palin',
+'Two Geese',
+'A Man in a Cat Suit'];
+
+const insertY = ["the neighbor's pool",
+'the town water fountain',
+'the toilet bowl'];
+
+const insertZ = ['melted like cotton candy',
+'evaporated',
+'peed in the public water facility'];
+
+// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+
+  const xItem = randomValueFromArray(insertX);
+const yItem = randomValueFromArray(insertY);
+const zItem = randomValueFromArray(insertZ);
+
+newStory = newStory.replaceAll(':insertx:', xItem);
+newStory = newStory.replaceAll(':inserty:', yItem);
+newStory = newStory.replaceAll(':insertz:', zItem);
+
+  if(customName.value !== 'Bob') {
+    const name = customName.value;
+    newStory = newStory.replaceAll('Bob', name);
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300/14) + ' stone';
+    const temperature =  Math.round((9/5)*94+32) + 'centigrade';
+
+    storyText = storyText.replaceAll('94 fahrenheit', temperature);
+    storyText = storyText.replaceAll('300 pounds', weight);
+
+
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
